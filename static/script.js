4fdc4218e8c2031e7ad43c1a324679f02933ba2f
Franklin Harvey <Franklinadamsharvey@gmail.com>
2017-01-27 14:12:24 -0700

    new index

---
+++ static/script.js
@@ -0,0 +1,54 @@
+(function() {
+		// Initialize Firebase
+var config = {
+		apiKey: "AIzaSyBbAS6KCerk3PJlzmxnYitajzij4cZ9CZU",
+		authDomain: "barnhouse-78075.firebaseapp.com",
+		databaseURL: "https://barnhouse-78075.firebaseio.com",
+		storageBucket: "barnhouse-78075.appspot.com",
+		messagingSenderId: "727310305344"
+	};
+	firebase.initializeApp(config);
+
+	const txtEmail = document.getElementById('txtEmail');
+	const txtFirstName = document.getElementById('txtFirstName');
+	const txtLastName = document.getElementById('txtLastName');
+	const btnSignUp = document.getElementById('btnSignUp');
+
+	btnSignUp.addEventListener('click', e => {
+		const email = txtEmail.value;
+		const firstName = txtFirstName.value;
+		const lastName = txtLastName.value;
+
+		if (email == "") {
+			alert("Please enter an email address");
+			return;
+		}
+		if (firstName == "") {
+			alert("Please enter an email address");
+			return;
+		}
+		if (lastName == "") {
+			alert("Please enter an email address");
+			return;
+		}
+		console.log("HWERE");
+		saveUser(email,firstName,lastName);
+		clearField(txtEmail,txtFirstName,txtLastName);
+	});
+
+	function saveUser(email,firstName,lastName) {
+		const entry = firstName+lastName;
+		console.log("HERE");
+		firebase.database().ref("EmailList/" + entry).set({
+			FirstName : firstName,
+			LastName : lastName,
+			Email : email
+		});
+	};
+	function clearField(txtEmail,txtFirstName,txtLastName) {
+			txtEmail.value = "";
+			txtFirstName.value = "";
+			txtLastName.value = "";
+	};
+
+}());
