5bdfa3a3fb7008baf9d74064564304fb79058926
Franklin Harvey <franklinadamsharvey@gmail.com>
2017-01-28 14:52:17 -0700

    update script

--- docs/static/script.js
+++ docs/static/script.js
@@ -46,20 +46,20 @@ var config = {
 		});
 	};
 	function clearField(txtEmail,txtFirstName,txtLastName) {
-			txtEmail.value = "";
-			txtFirstName.value = "";
-			txtLastName.value = "";
+		txtEmail.value = "";
+		txtFirstName.value = "";
+		txtLastName.value = "";
 	};
 
 
 	function guid() {
-  	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    	s4() + '-' + s4() + s4() + s4();
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+		s4() + '-' + s4() + s4() + s4();
 	};
 	function s4() {
-  	return Math.floor((1 + Math.random()) * 0x10000)
-    	.toString(16)
-    	.substring(1);
+		return Math.floor((1 + Math.random()) * 0x10000)
+		.toString(16)
+		.substring(1);
 	};
 
 }());
