372e89520c9dd2f1f652019834aeacee76843e69
Franklin Harvey <franklinadamsharvey@gmail.com>
2017-08-21 13:34:21 -0600

    Update script.js

--- docs/static/script.js
+++ docs/static/script.js
@@ -4,62 +4,52 @@ var config = {
 		apiKey: "AIzaSyBbAS6KCerk3PJlzmxnYitajzij4cZ9CZU",
 		authDomain: "barnhouse-78075.firebaseapp.com",
 		databaseURL: "https://barnhouse-78075.firebaseio.com",
+		projectId: "barnhouse-78075",
 		storageBucket: "barnhouse-78075.appspot.com",
 		messagingSenderId: "727310305344"
 	};
 	firebase.initializeApp(config);
+	var database = firebase.database();
 
 	const txtEmail = document.getElementById('txtEmail');
-	const txtFirstName = document.getElementById('txtFirstName');
-	const txtLastName = document.getElementById('txtLastName');
+	const txtName = document.getElementById('txtName');
+	const txtComments = document.getElementById('txtComments');
 	const btnSignUp = document.getElementById('btnSignUp');
 
 	btnSignUp.addEventListener('click', e => {
 		const email = txtEmail.value;
-		const firstName = txtFirstName.value;
-		const lastName = txtLastName.value;
+		const name = txtName.value;
+		const comments = txtComments.value;
 
 		if (email == "") {
 			alert("Please enter an email address");
 			return;
 		}
-		if (firstName == "") {
-			alert("Please enter an email address");
+		if (name == "") {
+			alert("Please enter your name");
 			return;
 		}
-		if (lastName == "") {
-			alert("Please enter an email address");
-			return;
-		}
-		saveUser(email,firstName,lastName);
-		clearField(txtEmail,txtFirstName,txtLastName);
+		saveUser(email,name,comments);
+		// clearField(email,name,comments);
 	});
 
-	function saveUser(email,firstName,lastName) {
-		const entry = guid();
+	function saveUser(email,name,comments) {
 		const eventTime = new Date();
+		const entry = firebase.database().ref("EmailList").push().key;
+
 		firebase.database().ref("EmailList/" + entry).set({
-			FirstName : firstName,
-			LastName : lastName,
-			Email : email,
-			DateAdded : eventTime.toString()
+			Name: name,
+			Comments: comments,
+			Email: email,
+			DateAdded: eventTime.toString()
 		});
-	};
-	function clearField(txtEmail,txtFirstName,txtLastName) {
-		txtEmail.value = "";
-		txtFirstName.value = "";
-		txtLastName.value = "";
-	};
 
-
-	function guid() {
-		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-		s4() + '-' + s4() + s4() + s4();
 	};
-	function s4() {
-		return Math.floor((1 + Math.random()) * 0x10000)
-		.toString(16)
-		.substring(1);
+	
+	function clearField(email,name,comments) {
+		email.value = "";
+		name.value = "";
+		comments.value = "";
 	};
 
 }());
