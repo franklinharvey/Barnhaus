c93b1054af5d8e02f218f035dd491fd601496a29
franklinharvey <franklinadamsharvey@gmail.com>
2017-10-07 18:00:00 -0600

    firebase loading of all products

--- docs/js/products.js
+++ docs/js/products.js
@@ -1,18 +1,72 @@
-var config = {
-  apiKey: "AIzaSyBbAS6KCerk3PJlzmxnYitajzij4cZ9CZU",
-  authDomain: "barnhouse-78075.firebaseapp.com",
-  databaseURL: "https://barnhouse-78075.firebaseio.com",
-  projectId: "barnhouse-78075",
-  storageBucket: "barnhouse-78075.appspot.com",
-  messagingSenderId: "727310305344"
-};
-firebase.initializeApp(config);
-var database = firebase.database().ref("Products");
-productsList = document.getElementById('productsList');
+$(function() {
+  var config = {
+    apiKey: "AIzaSyBbAS6KCerk3PJlzmxnYitajzij4cZ9CZU",
+    authDomain: "barnhouse-78075.firebaseapp.com",
+    databaseURL: "https://barnhouse-78075.firebaseio.com",
+    projectId: "barnhouse-78075",
+    storageBucket: "barnhouse-78075.appspot.com",
+    messagingSenderId: "727310305344"
+  };
+  firebase.initializeApp(config);
+  database = firebase.database().ref("Products");
+  productsList = document.getElementById('productsList');
+  pathArray = window.location.pathname.split( '/' );
+  itemType = pathArray[2];
+  console.log(itemType);
 
+  if (itemType == "shirts") {
+    database.orderByChild("Type").equalTo("shirt").on("child_added", function(snapshot) {
+      renderElements(snapshot, productsList);
+    });
+  }
+  else if (itemType == "sweatshirts") {
+    database.orderByChild("Type").equalTo("sweatshirt").on("child_added", function(snapshot) {
+      renderElements(snapshot, productsList);
+    });
+  }
+  else if (itemType == "hats") {
+    database.orderByChild("Type").equalTo("hat").on("child_added", function(snapshot) {
+      renderElements(snapshot, productsList);
+    });
+  }
+  else {
+    database.on('child_added', function(snapshot) {
+      renderElements(snapshot, productsList);
+    });
+  }
 
-database.on('value', function(snapshot) {
-  colDiv = document.createElement("div");
-  colDiv.className = "col-sm-4";
-  // updatePost(postElement, snapshot.val());
+  function renderElements(snapshot, productsList) {
+    colDiv = document.createElement("div");
+    colDiv.className = "col-sm-4";
+
+    linkTag = document.createElement("a");
+    linkTag.href = snapshot.val().buyURL;
+    linkTag.target = '_blank';
+
+    idAndClassDiv = document.createElement("div");
+    idAndClassDiv.id = snapshot.key;
+    idAndClassDiv.className = "product";
+    idAndClassDiv.style.backgroundImage = "url('" + snapshot.val().Image + "')";
+
+    productInfoDiv = document.createElement("div");
+    productInfoDiv.className = "product-info";
+
+    productName = document.createElement("h3");
+    productName.className = "product-name text-center";
+    productName.innerHTML = snapshot.val().Name;
+
+    productPrice = document.createElement("h4");
+    productPrice.className = "product-price text-center";
+    productPrice.innerHTML = snapshot.val().Price;
+
+    productInfoDiv.append(productName);
+    productInfoDiv.append(productPrice);
+
+    linkTag.append(idAndClassDiv);
+    linkTag.append(productInfoDiv);
+
+    colDiv.append(linkTag);
+
+    productsList.append(colDiv);
+  }
 });
